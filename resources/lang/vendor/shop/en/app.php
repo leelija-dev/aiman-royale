c9a868e4f7ca213ed692aabb6913522b46d4bc54
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2025-12-08 17:18:21 -0500

    admin layout and texts file app.php for english language is now updated

---
+++ resources/lang/vendor/shop/en/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'components' => [
+        'layouts' => [
+            'footer' => [
+                'footer-text' => '© Copyright 2025 - :current_year Leelija. All rights reserved.',
+            ],
+        ],
+    ],
+];
