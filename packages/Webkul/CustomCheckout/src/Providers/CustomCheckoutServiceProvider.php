78fd1dc2206878f47cf04b00c3620b3697368ca7
susmita <susmitaghosh.leelija@gmail.com>
2026-01-02 16:01:19 +0530

    admin address

--- packages/Webkul/CustomCheckout/src/Providers/CustomCheckoutServiceProvider.php
+++ packages/Webkul/CustomCheckout/src/Providers/CustomCheckoutServiceProvider.php
@@ -20,11 +20,26 @@ class CustomCheckoutServiceProvider extends ServiceProvider
         // Register shop namespace for overriding shop views with higher priority
         $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'shop');
         
+        // Register admin namespace for overriding admin views
+        $this->loadViewsFrom(__DIR__ . '/../Resources/views', 'admin');
+        
         // Override specific views by extending the view factory
         $this->app->booted(function () {
             $view = $this->app['view'];
+            
+            // Override shop customer address create form
             $view->composer('shop::customers.account.addresses.create', function ($view) {
                 $view->setPath(__DIR__ . '/../Resources/views/shop/customers/account/addresses/create.blade.php');
+            });
+            
+            // Override admin customer address create form
+            $view->composer('admin::customers.customers.view.address.create', function ($view) {
+                $view->setPath(__DIR__ . '/../Resources/views/admin/customers/customers/view/address/create.blade.php');
+            });
+            
+            // Override admin customer address edit form
+            $view->composer('admin::customers.customers.view.address.edit', function ($view) {
+                $view->setPath(__DIR__ . '/../Resources/views/admin/customers/customers/view/address/edit.blade.php');
             });
         });
     }
