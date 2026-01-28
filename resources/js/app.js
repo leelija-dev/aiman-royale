07b2a302c2f3a2d8be1d01f7da400bd130f8e565
Jayanta <jbleelija@gmail.com>
2026-01-28 17:23:11 +0530

    fix server site css issues

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import '../css/app.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '@fortawesome/fontawesome-free/js/all.min.js';
+// import '@fortawesome/fontawesome-free/js/all.min.js';
 // import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 // import 'summernote/dist/summernote-bs5.css';
 // import 'summernote/dist/summernote-bs5.js';
--- resources/views/layout/web/main-layout.blade.php
+++ resources/views/layout/web/main-layout.blade.php
@@ -46,8 +46,8 @@
       rel="stylesheet"
     />
     <!-- Font Awesome in  project -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" /> -->
 
 
     <!-- <link rel="stylesheet" href="web/css/staging.css"> -->
