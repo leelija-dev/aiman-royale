894b02ed2d5a4b125c5e559782749ca15bf608a2
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2026-02-09 17:54:12 +0530

    user guide modal added

--- resources/views/components/web/footer.blade.php
+++ resources/views/components/web/footer.blade.php
@@ -598,9 +598,7 @@
   });
 </script>
 
-<button type="button" data-size-guide-trigger class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-all active:scale-95">
-  View Size Guide
-</button>
+
 
 
 
