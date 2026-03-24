b56d5a1ae7af6badadbd53e0ac043b184854bdc1
susmita <susmitaghosh.leelija@gmail.com>
2026-03-24 13:14:28 +0530

    add sale in sidebar

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -10,7 +10,7 @@ $roles = $user->getRoleNames();
 
 $admin = Admin::find($userId);
 
-$productAndUnit = request()->routeIs('admin.categories.*','admin.unit','admin.add-unit','admin.colors', 'admin.products.*','admin.products-trashed','admin.occasions.index','admin.occasions.create','admin.occasions.edit','admin.occasions.trash','admin.products','admin.add-product','admin.unit.*', 'admin.brands.*', 'admin.colors.*', 'admin.sizes.*', 'admin.product-variants.*','admin.product-variants','admin.categories.create','admin.sizes', 'banners.*') ? true : false;
+$productAndUnit = request()->routeIs('admin.categories.*','admin.unit','admin.add-unit','admin.colors', 'admin.products.*','admin.products-trashed','admin.occasions.index','admin.occasions.create','admin.occasions.edit','admin.occasions.trash','admin.products','admin.add-product','admin.unit.*', 'admin.brands.*', 'admin.colors.*', 'admin.sizes.*', 'admin.product-variants.*','admin.product-variants','admin.categories.create','admin.sizes', 'banners.*', 'admin.sales.*') ? true : false;
 $isNewsletterActive = false;
 $isEmailActive = false;
 @endphp
@@ -278,6 +278,10 @@ $isEmailActive = false;
                         <li class="submenu-item">
                             <a class="submenu-link {{ request()->routeIs('admin.product-variants.*','admin.product-variants','admin.product-variants.create') ? 'active' : '' }} "
                                 href="{{ route('admin.product-variants') }}">Product Variants</a>
+                        </li>
+                        <li class="submenu-item">
+                            <a class="submenu-link {{ request()->routeIs('admin.sales.*') ? 'active' : '' }} "
+                                href="{{ route('admin.sales.index') }}">Sales (Fake Orders)</a>
                         </li>
                     </ul>
                 </div>
