b009bea793499d8c0d3675088e6b5ba9a1a3c97a
susmita <susmitaghosh.leelija@gmail.com>
2026-03-24 12:18:59 +0530

    migration file changes

--- database/migrations/2026_03_19_152947_create_banners_table.php
+++ database/migrations/2026_03_19_152947_create_banners_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('discount')->nullable();
             $table->string('button_text')->default('Shop Now');
             $table->string('filter')->unique();
-            $table->string('type')->default('main'); // 'main' or 'secondary'
+            $table->string('type')->default('main'); 
             $table->boolean('is_active')->default(true);
             $table->integer('sort_order')->default(0);
             $table->timestamps();
