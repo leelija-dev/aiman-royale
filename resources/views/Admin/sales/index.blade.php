2e5d65a61e0a9a006ddb45e412cafffb547e5845
susmita <susmitaghosh.leelija@gmail.com>
2026-03-27 16:06:58 +0530

    debug

--- resources/views/Admin/sales/index.blade.php
+++ resources/views/Admin/sales/index.blade.php
@@ -20,6 +20,52 @@
         </div>
     </div>
 
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Fake Orders List</h3>
+                            <div class="card-tools">
+                                <a href="{{ route('admin.sales.create') }}" class="btn btn-primary btn-sm">
+                                    <i class="fas fa-plus"></i> Create Fake Order
+                                </a>
+                                <span class="badge badge-info">Total: {{ $sales->total() }}</span>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            @if(session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                            @endif
+
+                            <table class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Order ID</th>
+                                        <th>Customer</th>
+                                        <th>Products</th>
+                                        <th>Total Amount</th>
+                                        <th>Status</th>
+                                        <th>Created At</th>
+                                        <!-- <th>Actions</th> -->
+                                    </tr>
+                                </thead>
+                                
+                        </table>
+
+
+                        {{ $sales->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+</section>
+
+
 
 </div>
 @endsection
