c18891ae92c36a90ebae1f1702c42dc2f52e2d8d
susmita <susmitaghosh.leelija@gmail.com>
2026-07-29 15:16:30 +0530

    bridal and wedding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,10 +8,12 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\URL;
 use App\Models\Notification;
 use App\Models\Category;
+use App\Models\Occasion;
 use App\Models\Size;
 use App\Models\Wishlist;
 use App\Models\Product;
 use App\Models\ProductVariant;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -35,26 +37,37 @@ class AppServiceProvider extends ServiceProvider
             return $user->hasRole('superadmin') ? true : null;
         });
         View::composer('*', function ($view) {
-        $notifications = Notification::where('viewed', 0)->latest()->get();
-        $categories = Category::where('is_active', 1)->with('products')->orderBy('name')->get();
-        $productCategory = ProductVariant::with(['product.category','product.images', 'images'])
-    ->get()
-    ->unique(function ($variant) {
-        return optional($variant->product)->category_id;
-    })
-    ->values();
-        $sizes=Size::OrderBy('sort_order')->get();
-        
-        
-        // Only get wishlists if user is authenticated
-        $wishlists = [];
-        if (auth()->check()) {
-            $wishlists = Wishlist::where('user_id', auth()->user()->id)->with(['product.images', 'variant'])
-                            ->get();
-        }
-        
-        $view->with('notifications', $notifications)->with('categories', $categories)->with('sizes', $sizes)
-        ->with('wishlists', $wishlists)->with('productCategory', $productCategory);
-    });
-   }
+            $notifications = Notification::where('viewed', 0)->latest()->get();
+            $categories = Category::where('is_active', 1)->with('products')->orderBy('name')->get();
+            $wedding = Occasion::where('slug', 'wedding')->first();
+            $wedding_id = $wedding->id ?? 0;
+            $bridal = Occasion::where('slug', 'bridal')->first();
+            $bridal_id = $bridal->id ?? 0;
+            $categoriesWithWedding = 
+            // $weddingProducts = DB::table('products')
+            //     ->where('ocassion_id', $wedding_id)
+            //     ->get();
+            $weddingProducts = Product::where('ocassion_id', $wedding_id)->get();
+            $bridalProducts = Product::where('ocassion_id', $bridal_id)->get();
+            // dd($weddingProducts);
+            $productCategory = ProductVariant::with(['product.category', 'product.images', 'images'])
+                ->get()
+                ->unique(function ($variant) {
+                    return optional($variant->product)->category_id;
+                })
+                ->values();
+            $sizes = Size::OrderBy('sort_order')->get();
+
+
+            // Only get wishlists if user is authenticated
+            $wishlists = [];
+            if (auth()->check()) {
+                $wishlists = Wishlist::where('user_id', auth()->user()->id)->with(['product.images', 'variant'])
+                    ->get();
+            }
+
+            $view->with('notifications', $notifications)->with('categories', $categories)->with('sizes', $sizes)
+                ->with('wishlists', $wishlists)->with('productCategory', $productCategory);
+        });
+    }
 }
