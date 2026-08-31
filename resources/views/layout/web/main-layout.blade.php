14961ccceb5577d79a345121bb02bb5048a3f56a
Lakshman Pal <lakshman.leelija@gmail.com>
2026-08-31 16:02:17 +0530

    update main layout

--- resources/views/layout/web/main-layout.blade.php
+++ resources/views/layout/web/main-layout.blade.php
@@ -134,30 +134,6 @@
         })(window, document, 'script', 'dataLayer', 'GTM-5MP8JR47');
     </script>
 
-    <script>
-        ! function(f, b, e, v, n, t, s) {
-            if (f.fbq) return;
-            n = f.fbq = function() {
-                n.callMethod ?
-                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-            };
-            if (!f._fbq) f._fbq = n;
-            n.push = n;
-            n.loaded = !0;
-            n.version = '2.0';
-            n.queue = [];
-            t = b.createElement(e);
-            t.async = !0;
-            t.src = v;
-            s = b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t, s)
-        }(window, document, 'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1931344804203776');
-        fbq('track', 'PageView');
-    </script>
-    <noscript><img height="1" width="1" style="display:none"
-            src="https://www.facebook.com/tr?id=1931344804203776&ev=PageView&noscript=1" /></noscript>
 
 </head>
 
