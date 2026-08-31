4f68b6140675ec70ffff2f7a40947e3d70c484da
susmita <susmitaghosh.leelija@gmail.com>
2026-08-31 10:46:22 +0530

    schema issue

--- resources/views/layout/web/main-layout.blade.php
+++ resources/views/layout/web/main-layout.blade.php
@@ -51,8 +51,12 @@
 
     <!-- Dynamic Schema Markup -->
     @if(isset($pageMeta->schema_markup) && !empty($pageMeta->schema_markup))
-       {!! $pageMeta->schema_markup !!}
-    
+    <script type="application/ld+json">
+        {
+            !!$pageMeta - > schema_markup!!
+        }
+    </script>
+
     @endif
 
     <!-- PWA Manifest - FIXED PATH -->
