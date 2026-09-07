7c8b2856b13e9afcbcaa0e701b1fa9624f175efd
susmita <susmitaghosh.leelija@gmail.com>
2026-09-07 18:14:42 +0530

    wish;ist

--- resources/views/web/wishlist.blade.php
+++ resources/views/web/wishlist.blade.php
@@ -198,7 +198,7 @@
                             <a href="{{ route('page.single-product', $product->slug) }}">
                                 <div class="h-64 w-full bg-gradient-to-br from-purple-100 to-pink-100 flex items-center justify-center ">
                                     @if($product->featured_image)
-                                    <img src="{{ asset($product->featured_image) }}"
+                                    <img src="{{ url('/img/' . $product->featured_image) }}"
                                         alt="{{ $product->name }}"
                                         class="h-full w-full object-cover">
                                     @else
