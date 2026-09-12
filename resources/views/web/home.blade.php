b7707643dfe1b4358aa6398974c088c8e503b78a
susmita <susmitaghosh.leelija@gmail.com>
2026-09-12 13:57:02 +0530

    category image

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -110,9 +110,10 @@
                         if (strpos($catImage, 'cloudinary.com') !== false && strpos($catImage, 'upload/') !== false) {
                         $parts = explode('upload/', $catImage);
                         $catImage = $parts[0] . 'upload/w_550,h_800,c_fill,f_auto,q_auto/' . $parts[1];
-                        } else{
-                            $catImage = 'img/' . $catImage . '?w=600&q=80';
-                        }
+                        } 
+                       // else{
+                       //     $catImage = 'img/' . $catImage . '?w=600&q=80';
+                       // }
                         @endphp
                         <img src="{{ $catImage }}"
                             alt="{{ $category->product->category->name }}"
