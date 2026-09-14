8a1f2952d49585f32bf60ad489f7336016e60260
Lakshman Pal <lakshman.leelija@gmail.com>
2026-09-14 11:37:23 +0530

    update main layout

--- resources/views/layout/web/main-layout.blade.php
+++ resources/views/layout/web/main-layout.blade.php
@@ -98,12 +98,12 @@
     <!-- <link rel="stylesheet" href="web/css/staging.css"> -->
 
     <!-- Owl Carousel CSS -->
-    {{-- <link rel="stylesheet"
+    <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" /> --}}
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
         {{--this to --}}
-        <link rel="preload"
+        {{-- <link rel="preload"
             as="style"
             href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
             onload="this.onload=null;this.rel='stylesheet'">
@@ -118,7 +118,7 @@
                 href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
             <link rel="stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
-        </noscript> 
+        </noscript>  --}}
         <!-- end this -->
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"> -->
 
