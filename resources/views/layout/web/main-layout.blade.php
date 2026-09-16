5ecf5d4d698366b6e3a26e8c44359f268fdf52e9
susmita <susmitaghosh.leelija@gmail.com>
2026-09-16 16:07:59 +0530

    changes

--- resources/views/layout/web/main-layout.blade.php
+++ resources/views/layout/web/main-layout.blade.php
@@ -203,7 +203,25 @@
     <!-- <link rel="stylesheet" href="{{ asset('web/css/app-popup.css') }}"> -->
 
     <!-- GTM: load async, don't block render -->
-    <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-5MP8JR47"></script>
+     <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.defer = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-5MP8JR47');
+    </script>
+     
+    <!-- <script async src="https://www.googletagmanager.com/gtm.js?id=GTM-5MP8JR47"></script> -->
     <script>
         window.addEventListener('load', function() {
         ! function(f, b, e, v, n, t, s) {
