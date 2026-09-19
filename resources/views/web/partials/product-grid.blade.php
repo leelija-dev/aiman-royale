0cd9e211d5527ba723319b59577dd0e937c05505
susmita <susmitaghosh.leelija@gmail.com>
2026-09-19 10:56:44 +0530

    debug

--- resources/views/web/partials/product-grid.blade.php
+++ resources/views/web/partials/product-grid.blade.php
@@ -21,6 +21,7 @@
             }
 
             if (!empty($images)) {
+                dd( $images);
                 if (is_array($images)) {
                     // If it's an array of objects/arrays
                     $firstImage = $images[0] ?? null;
