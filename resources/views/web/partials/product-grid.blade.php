11008db049f7f4b7c4e64910a319ecdbd145514d
susmita <susmitaghosh.leelija@gmail.com>
2026-09-19 11:06:08 +0530

    debug

--- resources/views/web/partials/product-grid.blade.php
+++ resources/views/web/partials/product-grid.blade.php
@@ -65,7 +65,7 @@
                 $parts = explode('upload/', $imageUrl);
                 $imageUrl = $parts[0] . 'upload/w_600,h_900,c_fill,f_auto,q_auto,dpr_auto/' . $parts[1];
             }
-
+dd($imageUrl);
             // Get product data (works for both object and array)
             $isFeatured = is_object($product) ? ($product->is_featured ?? false) : ($product['is_featured'] ?? false);
             $price = is_object($product) ? ($product->price ?? 0) : ($product['price'] ?? 0);
