f19b67610f9cdb24346c4d3d9c8b6cf7b90fe5f2
susmita <susmitaghosh.leelija@gmail.com>
2026-09-21 10:55:40 +0530

    capcha

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -103,7 +103,7 @@ class AuthController extends Controller
             'name'     => 'required|string|max:255',
             'email'    => 'required|string|min:10|max:255', // "email" here = combined email/phone field
             'password' => 'required|min:6',
-            // 'cf-turnstile-response' => ['required', new Turnstile],
+            'cf-turnstile-response' => ['required', new Turnstile],
         ]);
 
         // 2. Detect if input is email or phone
